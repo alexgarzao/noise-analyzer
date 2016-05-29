b8c2f39d97e1cd05fa07aa8faa28ac82b226b8ea
Alex S. Garzao <alexgarzao@gmail.com>
2016-05-29 18:50:03 -0300

    Docstrings and () were removed at the class definition.

--- noise_analyzer/NoiseWaveAnalyzer.py
+++ noise_analyzer/NoiseWaveAnalyzer.py
@@ -3,8 +3,16 @@
 # Author: Alex S. Garzão <alexgarzao@gmail.com>
 # NoiseWaveAnalyzer.py
 
-class NoiseWaveAnalyzer():
+class NoiseWaveAnalyzer:
+	'''NoiseWaveAnalyzer receives a threshold and, based on this value, can decide if the data is a good signal or a noise.
+	'''
+
 	def __init__(self, threshold):
+		'''Initialize the object.
+
+		threshold: is the limit to decide if a data is a wave or a noise.
+		'''
 		self.Threshold = threshold
+
 	def IsNoise(self, value):
 		return value <= self.Threshold
