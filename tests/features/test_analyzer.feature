915add0a2e1f777f4b20eb615b8aaa28570ee324
Alex S. Garzao <alexgarzao@gmail.com>
2016-05-31 15:47:08 -0300

    Tests to valid that the noise file is correct.
    Basically a file is passed and the result noise file is checked.
    
    Issue #15.

--- tests/features/test_analyzer.feature
+++ tests/features/test_analyzer.feature
@@ -2,7 +2,12 @@ Feature: Test with recorded data in CVS files
   In order to test analyzer class
   We'll use some recorded data stored in CSV files
 
-  Scenario: Testing with original sample
-    Given I have the file "230V_50Hz_ADCdiv1"
+  Scenario: Testing with the original sample
+    Given I have the file "230V_50Hz_ADCdiv1.csv"
     When I send this file to analyzer
-    Then I see noise values equals to file "230V_50Hz_ADCdiv1.noise"
+    Then I see noise values in file "../noise_connection__1.csv" equals to file "230V_50Hz_ADCdiv1.noise.csv"
+
+  Scenario: Testing again with the original sample
+    Given I have the file "230V_50Hz_ADCdiv1.csv"
+    When I send this file to analyzer
+    Then I see noise values in file "../noise_connection__2.csv" equals to file "230V_50Hz_ADCdiv1.noise.csv"
--- tests/features/test_analyzer_steps.py
+++ tests/features/test_analyzer_steps.py
@@ -1,11 +1,14 @@
 import socket
 import sys
+import time
+
+import filecmp
 
 from lettuce import *
 
 @step('I have the file "(.*)"')
 def have_the_file(step, filename):
-    world.filename = filename + '.csv'
+    world.filename = filename
 
 @step('I send this file to analyzer')
 def send_this_file_to_analyzer(step):
@@ -20,9 +23,13 @@ def send_this_file_to_analyzer(step):
 
     s.close()
 
-@step('I see noise values equals to file "(.*)"')
-def see_noise_values_equals_to_file(step, filename):
-    pass
+@step('I see noise values in file "(.*)" equals to file "(.*)"')
+def see_noise_values_equals_to_file(step, noise_filename, expected_noise_filename):
+    # need time to file be flushed :-)
+    time.sleep(5)
+
+    assert filecmp.cmp(noise_filename, expected_noise_filename) == True, \
+        "Noise file %s differ from expected file %s" % (noise_filename, expected_noise_filename)
 
 def factorial(number):
     number = int(number)
